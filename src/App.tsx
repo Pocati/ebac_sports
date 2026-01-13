5f0e2c20d611c4d5e8930bea9c24abc4f657743e
Pocati <hipocrates1555@gmail.com>
2026-01-12 22:04:58 -0300

    Trocando requisição state/effect por redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
 import { useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
+
 import { addToCart } from './store/carrinhoSlice'
+import { favoritar } from './store/favoritosSlice'
 import { useGetProdutosQuery } from './store/produtosAPI'
 import { Produto } from './types'
 
 import { GlobalStyle } from './styles'
 
 function App() {
-  const { data: produtos = [], isLoading } = useGetProdutosQuery()
   const dispatch = useDispatch<AppDispatch>()
+
+  const { data: produtos = [], isLoading } = useGetProdutosQuery()
   const carrinho = useSelector((state: RootState) => state.cart.items)
-
-  const [favoritos, setFavoritos] = useState<Produto[]>([])
-
+  const favoritos = useSelector((state: RootState) => state.favoritos.items)
   function adicionarAoCarrinho(produto: Produto) {
     if (carrinho.find((p) => p.id === produto.id)) {
       alert('Item já adicionado')
@@ -24,13 +24,8 @@ function App() {
     }
   }
 
-  function favoritar(produto: Produto) {
-    if (favoritos.find((p) => p.id === produto.id)) {
-      const favoritosSemProduto = favoritos.filter((p) => p.id !== produto.id)
-      setFavoritos(favoritosSemProduto)
-    } else {
-      setFavoritos([...favoritos, produto])
-    }
+  function handleFavoritar(produto: Produto) {
+    dispatch(favoritar(produto))
   }
 
   return (
@@ -44,7 +39,7 @@ function App() {
           <Produtos
             produtos={produtos}
             favoritos={favoritos}
-            favoritar={favoritar}
+            favoritar={handleFavoritar}
             adicionarAoCarrinho={adicionarAoCarrinho}
           />
         )}
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './carrinhoSlice'
 import { produtosApi } from './produtosAPI'
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+import favoritosReducer from './favoritosSlice'
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    favoritos: favoritosReducer,
     [produtosApi.reducerPath]: produtosApi.reducer
   },
   middleware: (getDefaultMiddleware) =>
